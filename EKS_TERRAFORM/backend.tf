16761f27474fc155070944b60b9a6d0465581485
thiru309 <151728309+thiru309@users.noreply.github.com>
2024-04-15 14:54:19 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,16 @@
 terraform {
   backend "s3" {
-    bucket = "madhu-vcube1" # Replace with your actual S3 bucket name
+    bucket = "
+Table Selection	
+Name
+	
+AWS Region
+	
+IAM Access Analyzer
+	
+Creation date
+
+jenkinsbucket123" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
